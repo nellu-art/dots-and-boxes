2fadd49340a49689e23426e6a33f1fdfb89dce87
Ravil <ravil.nell@gmail.com>
2023-04-29 06:29:22 +0400

    mark complete cells by players color

--- src/GameBoard.tsx
+++ src/GameBoard.tsx
@@ -1,3 +1,4 @@
+import { createUseStyles } from 'react-jss';
 import { useState } from 'react';
 import './GameBoard.css';
 
@@ -43,6 +44,52 @@ const getPlayersColor = (playerId: string) => {
   }
 };
 
+function getCellNeighbors({
+  board,
+  rowIndex,
+  colIndex,
+}: {
+  board: Board;
+  rowIndex: number;
+  colIndex: number;
+}) {
+  const neighbors: {
+    top: CellData | null;
+    bottom: CellData | null;
+    left: CellData | null;
+    right: CellData | null;
+  } = {
+    top: null,
+    bottom: null,
+    left: null,
+    right: null,
+  };
+
+  if (rowIndex > 0) {
+    neighbors.top = board[rowIndex - 1][colIndex];
+  }
+
+  if (rowIndex < boardSize - 1) {
+    neighbors.bottom = board[rowIndex + 1][colIndex];
+  }
+
+  if (colIndex > 0) {
+    neighbors.left = board[rowIndex][colIndex - 1];
+  }
+
+  if (colIndex < boardSize - 1) {
+    neighbors.right = board[rowIndex][colIndex + 1];
+  }
+
+  return neighbors;
+}
+
+function isCellCompleted(cell: CellData) {
+  const cellBorders = { top: cell.top, bottom: cell.bottom, left: cell.left, right: cell.right };
+
+  return Object.values(cellBorders).every((value) => value !== null);
+}
+
 export const GameBoard = () => {
   const [activePlayer, setActivePlayer] = useState<string>(PLAYERS_ID.PLAYER_1);
   const [board, setBoard] = useState<Board>(initialBoardData);
@@ -54,30 +101,54 @@ export const GameBoard = () => {
 
     if (cell[position] !== null) return;
 
+    const cellNeighbors = getCellNeighbors({ board: newState, rowIndex, colIndex });
+
     switch (position) {
       case 'top':
         cell.top = activePlayer;
+
+        if (cellNeighbors.top) {
+          cellNeighbors.top.bottom = activePlayer;
+        }
+
         break;
       case 'bottom':
         cell.bottom = activePlayer;
+
         break;
       case 'left':
         cell.left = activePlayer;
+
+        if (cellNeighbors.left) {
+          cellNeighbors.left.right = activePlayer;
+        }
+
         break;
       case 'right':
         cell.right = activePlayer;
+
         break;
     }
 
-    const hasAllSelectedLine = Object.values(cell).every((value) => value !== null);
-
-    if (hasAllSelectedLine) {
+    if (!cell.completedBy && isCellCompleted(cell)) {
       cell.completedBy = activePlayer;
-    } else {
-      setActivePlayer((prevState) =>
-        prevState === PLAYERS_ID.PLAYER_1 ? PLAYERS_ID.PLAYER_2 : PLAYERS_ID.PLAYER_1
-      );
+    } else if (
+      cellNeighbors.top &&
+      !cellNeighbors.top.completedBy &&
+      isCellCompleted(cellNeighbors.top)
+    ) {
+      cellNeighbors.top.completedBy = activePlayer;
+    } else if (
+      cellNeighbors.left &&
+      !cellNeighbors.left.completedBy &&
+      isCellCompleted(cellNeighbors.left)
+    ) {
+      cellNeighbors.left.completedBy = activePlayer;
     }
+
+    setActivePlayer((prevState) =>
+      prevState === PLAYERS_ID.PLAYER_1 ? PLAYERS_ID.PLAYER_2 : PLAYERS_ID.PLAYER_1
+    );
 
     setBoard(newState);
   };
@@ -113,19 +184,24 @@ interface CellProps {
   data: CellData;
 }
 
+const useCellStyles = createUseStyles<'completedBoxColor', { color?: string }>({
+  completedBoxColor: {
+    '&::after': {
+      backgroundColor: ({ color }) => color,
+    },
+  },
+});
+
 const Cell = ({ rowIndex, colIndex, showRight, showBottom, onClick, data }: CellProps) => {
+  const completedByColor = getPlayersColor(data.completedBy || '');
+  const classes = useCellStyles({ color: completedByColor });
+
   const handleClick = (position: Position) => {
     onClick({ rowIndex, colIndex, position });
   };
 
-  // const getCompletedClassName = () => {
-  //   if (data.completedBy === 1) return 'player1';
-  //   if (data.completedBy === 2) return 'player2';
-  //   return '';
-  // };
-
   return (
-    <div className={`cell`}>
+    <div className={`cell ${completedByColor ? 'completed' : ''} ${classes.completedBoxColor}`}>
       <Line position='top' selectedBy={data.top || undefined} onClick={() => handleClick('top')} />
       <Dot />
       <Line
